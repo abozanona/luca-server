e811711b7be01028850817f39629a312fd43fb5f
abozanona <abozanona@gmail.com>
2022-05-13 21:08:56 +0300

    Add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [{
+    name: "luca",
+    script: "./index.js",
+    instances: "max",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+};
